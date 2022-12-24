f7612dd09225c854802e34978e6ba0f4f1dc7956
modfayz <modfayz@gmail.com>
2022-12-24 14:31:10 +0100

    ajouter les modules/migration/seeders

---
+++ src/Models/Pays.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Aitbella\Cosmed\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Pays extends Model
+{
+    use HasFactory;
+}
---
+++ src/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Aitbella\Cosmed\database\seeders;
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->call([
+            PaysSeeder::class,
+            DestinataireSeeder::class,
+        ]);
+    }
+}
